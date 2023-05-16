c4de1e82e431ca532d6869b3a1bde6502b2f2844
Lucky-Martin <luckymartinlc@gmail.com>
2023-05-16 16:18:38 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qr-scanner';
+  qrData!: string;
+
+  onQRScanned(qrString: string) {
+    this.qrData = qrString;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ZXingScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
